83f1f91d6d698d29d4f7c71c22383aa6f8a099f8
BrianFaviansa <brianfpd31@gmail.com>
2024-11-12 19:45:13 +0700

    feat: main activity text

--- app/src/main/java/com/c242_ps302/sehatin/ui/MainActivity.kt
+++ app/src/main/java/com/c242_ps302/sehatin/ui/MainActivity.kt
@@ -8,9 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.c242_ps302.sehatin.ui.theme.SehatinTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +18,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             SehatinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    Text(
+                        modifier = Modifier.padding(innerPadding),
+                        text = "Hello, Sehatin!"
                     )
                 }
             }
@@ -30,18 +28,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SehatinTheme {
-        Greeting("Android")
-    }
-}
